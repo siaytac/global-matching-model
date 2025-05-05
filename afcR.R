45623f3a0bddd3841b28c179d119909fa8dd7d94
Sinem Aytac <70482578+siaytac@users.noreply.github.com>
2025-05-05 11:48:07 -0400

    Delete afcR.R

--- afcR.R
+++
@@ -1,7 +0,0 @@
-
-#Find the accuracy for the AFC task when there is one new and (k-1) old items
-afcR <- function(mu1,sd1,mu2,sd2,k){
-
-  integrate(function(x,mu1,sd1,mu2,sd2) dnorm(x,mu2,sd2)*(1-pnorm(x,mu1,sd1))^(k-1), -Inf, Inf, mu1=mu1, sd1=sd1, mu2=mu2, sd2=sd2)$value
-
-}
