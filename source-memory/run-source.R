76ca2d8c27d0afa4a6b4ea92f0b62c44b03418e3
Sinem Aytac <70482578+siaytac@users.noreply.github.com>
2025-05-05 11:07:23 -0400

    run-source.R

--- source-memory/run-source.R
+++ source-memory/run-source.R
@@ -8,9 +8,10 @@ source("model-source.R") # Osth et al. (2018) Source Memory Model
 source("lcu.R") # the function to find criterion on LR scale 
 source("tpe-source.R") # test position effect for source memory
 
-#convert d-prime to accuracy
+# convert d-prime to accuracy
 kafc <- function(d,k) integrate(function(x,d,k) dnorm(x,d)*pnorm(x)**(k-1), -Inf, Inf, d=d,k=k)$value
 
+# set the parameters                             
 ll <- 48 # study list length
 lltest <- ll*2 # test list length
 nitem <- 500 # number of prior occurrences of items (would be different for low- vs. high-freq words)
@@ -23,4 +24,5 @@ rsource <- 0.5 # learning rate for context/source
 
 hr <- fr <- pc <- c() # save the model's predictions
 
+# run the simulation                           
 simulations <- tpeSource(ll=ll, lltest=lltest, ritem=ritem, rsource=rsource, nitem=nitem, Mss=Mss, contextDrift=contextDrift, k=k)
