2576a36e123a680459af212b42db625af1bbc854
Sinem Aytac <70482578+siaytac@users.noreply.github.com>
2025-05-19 12:22:29 -0400

    Update tpe-source.R

--- source-memory/tpe-source.R
+++ source-memory/tpe-source.R
@@ -1,9 +1,9 @@
-tpeSource <- function(ll, lltest, ritem, rsource, nitem, Mss, contextDrift, k) {
+tpeSource <- function(ll, lltest, ritem, rsource, nitem, k, Mss, contextDrift, Mtt, Maa, Mbb, Vss, Vtt, Vti, Vsu, Vaa, Vbb, Vab, Vba, Vac, Vbc) {
 
   for (i in 1:(lltest)) { 
     
-    d <- osthSource(l=ll, ritem=ritem, rsource=rsource, nitem=nitem, Mss=Mss, 
-                    Mtt=1, Maa=0.6, Mbb=0.6, Vss=.1, Vtt=.05, Vti=.001, Vsu=.0001, Vaa=.01, Vbb=.01, Vab=.01, Vba=.01, Vac=.25, Vbc=.25)
+    d <- osthSource(l=ll, ritem=ritem, rsource=rsource, nitem=nitem, Mss=Mss, Mtt=Mtt, Maa=Maa, Mbb=Mbb, 
+                    Vss=Vss, Vtt=Vtt, Vti=Vti, Vsu=Vsu, Vaa=Vaa, Vbb=Vbb, Vab=Vab, Vba=Vba, Vac=Vac, Vbc=Vbc)
     
     #item recognition
     muo <- d[1]/sqrt(d[4])
